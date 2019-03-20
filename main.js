d0b258e6d488efabb61ee68314d1e330a3b57215
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-03-19 21:05:06 -0500

    usin bootstrap

---
+++ main.js
@@ -0,0 +1,89 @@
+const products = [
+    {
+        title: 'product1',
+        description: 'sumsum',
+        imageUrl: 'links.com',
+        size: '4"x4"',
+        weight: '8oz.',
+        price1: 'costs',
+        price2: 'costs',
+        price3: 'costs',
+        validUntil: '04/01/2016'
+    },
+    {
+        title: 'product2',
+        description: 'sumsum',
+        imageUrl: 'links.com',
+        size: '4"x4"',
+        weight: '8oz.',
+        price1: 'costs',
+        price2: 'costs',
+        price3: 'costs',
+        validUntil: '04/01/2016'
+    },
+    {
+        title: 'product3',
+        description: 'sumsum',
+        imageUrl: 'links.com',
+        size: '4"x4"',
+        weight: '8oz.',
+        price1: 'costs',
+        price2: 'costs',
+        price3: 'costs',
+        validUntil: '04/01/2016'
+    },
+    {
+        title: 'product4',
+        description: 'sumsum',
+        imageUrl: 'links.com',
+        size: '4"x4"',
+        weight: '8oz.',
+        price1: 'costs',
+        price2: 'costs',
+        price3: 'costs',
+        validUntil: '04/01/2016'
+    },
+    {
+        title: 'product5',
+        description: 'sumsum',
+        imageUrl: 'links.com',
+        size: '4"x4"',
+        weight: '8oz.',
+        price1: 'costs',
+        price2: 'costs',
+        price3: 'costs',
+        validUntil: '04/01/2016'
+    },
+];
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const ProdBuilder = (cards) => {
+    let domString = '';
+        // alt. method domString += '<div class="row">';
+    cards.forEach((product) => {
+        domString += `<div class="col-4 product">`;
+        domString += `<h2>${product.title}</h2>`;
+        // domString += `<p>${product.description}</p>`;
+        // domString += `<p>${product.imageUrl}</p>`;
+        // domString += `<p>${product.size}</p>`;
+        // domString += `<p>${product.weight}</p>`;
+        // domString += `<p>${product.price1}</p>`;
+        // domString += `<p>${product.price2}</p>`;
+        // domString += `<p>${product.price3}</p>`;
+        // domString += `<p>${product.validUntil}</p>`;
+        domString += `</div>`;
+    })
+    // alt method domString += '</div>';
+    printToDom('page', domString);
+}
+
+
+const init = () => {
+ ProdBuilder(products);
+};
+
+init()
